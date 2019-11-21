966f06111b4420e08d0e5c5eb5d4b62e9f87f6b5
fxlmo <josh.felmeden@outlook.com>
2019-11-21 14:13:27 +0000

    attempt 1 for 1b

--- gol.go
+++ gol.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -25,14 +24,30 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 		for x := 0; x < p.imageWidth; x++ {
 			val := <-d.io.inputVal
 			if val != 0 {
-				fmt.Println("Alive cell at", x, y)
 				world[y][x] = val
 			}
 		}
 	}
 
 	//call world building function
-	world = calcPgm(p, d, alive, world)
+	var chans []chan [][]byte
+	for c := 0; c < p.threads; c++ {
+		chans = append(chans, make(chan [][]byte))
+	}
+
+	for w := 1; w <= p.threads; w++ {
+		go calcPgm(p, d, alive, world, w, chans[w-1])
+	}
+
+	//function to reconstruct world
+	for w := 0; w < p.threads; w++ {
+		worldPart :=<-chans[w]
+		for y := 0; y < p.imageHeight/p.threads; y++ {
+			for x := 0; x < p.imageWidth; x++ {
+				world[y + w*(p.imageHeight/p.threads)][x] = worldPart[y % (p.imageHeight/p.threads)][x]
+			}
+		}
+	}
 
 	// Create an empty slice to store coordinates of cells that are still alive after p.turns are done.
 	var finalAlive []cell
@@ -53,16 +68,16 @@ func distributor(p golParams, d distributorChans, alive chan []cell) {
 	alive <- finalAlive
 }
 
-func calcPgm(p golParams, d distributorChans, alive chan []cell, world [][]byte) [][]byte {
+func calcPgm(p golParams, d distributorChans, alive chan []cell, world [][]byte, id int, channel chan [][]byte) {
 	// initiate temporary world structure
-	tempWorld := make([][]int, p.imageHeight)
+	tempWorld := make([][]int, p.imageHeight/p.threads)
 	for i := range tempWorld {
 		tempWorld[i] = make([]int, p.imageWidth)
 	}
 
 	// Calculate the new state of Game of Life after the given number of turns.
 	for turns := 0; turns < p.turns; turns++ {
-		for y := 0; y < p.imageHeight; y++ {
+		for y := (id-1)*p.imageHeight/p.threads; y < id*p.imageHeight/p.threads; y++ {
 			for x := 0; x < p.imageWidth; x++ {
 				// collect neighbours
 				liveNeighbours := 0
@@ -70,7 +85,7 @@ func calcPgm(p golParams, d distributorChans, alive chan []cell, world [][]byte)
 					for j := -1; j <= 1; j++ {
 						if j != 0 || i != 0 {
 							a, b := y+i, x+j
-							a = (a + p.imageHeight) % (p.imageHeight)
+							a = (a + id*p.imageHeight/p.threads) % (id*p.imageHeight/p.threads)
 							b = (b + p.imageWidth) % (p.imageWidth)
 							if world[a][b] == 0xFF {
 								liveNeighbours += 1
@@ -78,20 +93,19 @@ func calcPgm(p golParams, d distributorChans, alive chan []cell, world [][]byte)
 						}
 					}
 				}
-				tempWorld[y][x] = liveNeighbours
+				tempWorld[y % (p.imageHeight/p.threads)][x] = liveNeighbours
 			}
 		}
-		for a := 0; a < p.imageHeight; a++ {
+		for a := 0; a < id*p.imageHeight/p.threads; a++ {
 			for b := 0; b < p.imageWidth; b++ {
-				if tempWorld[a][b] == 3 {
+				if tempWorld[a % (p.imageHeight/p.threads)][b] == 3 {
 					world[a][b] = 0xFF
-				} else if tempWorld[a][b] != 2 {
+				} else if tempWorld[a% (p.imageHeight/p.threads)][b] != 2 {
 					world[a][b] = 0
 				}
-				tempWorld[a][b] = 0
+				tempWorld[a % (p.imageHeight/p.threads)][b] = 0
 			}
 		}
 	}
-
-	return world
+	channel <-world
 }
